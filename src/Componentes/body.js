d79daaac39b777e86ce6ae013f7ad9a73fcf6934
rebecafrutuoso <108592549+rebecafrutuoso@users.noreply.github.com>
2022-11-04 23:39:04 -0300

    Ajuste GitHub

---
+++ src/Componentes/body.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function body() {
+  return (
+    <section>
+      <h2> teste</h2>
+      <h5> teste 3</h5>
+    </section>
+  );
+}
---
+++ src/Componentes/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Logo from "./imagens/logocabeçalho.png";
+export default function Header() {
+  return (
+    <Header>
+      <img src={Logo} />
+      <h1> TESTE 123</h1>
+    </Header>
+  );
+}
